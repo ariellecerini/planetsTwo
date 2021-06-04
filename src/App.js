5a015f9864ef469753760ea5c827e06a4ede257b
ariellecerini <arielle@acerinidesigns.com>
2021-06-04 17:31:55 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./App.css";
+import {PlanetName} from './starWars'
+
+function App() {
+  return (
+    <div className="App">
+     
+        <PlanetName />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/starWars.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import "./App.css";
+
+
+class PlanetName extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+        error : null,
+        isLoaded : false,
+        planet : []        
+    };
+}
+  componentDidMount() {
+    fetch("http://swapi.dev/api/planets/")
+    .then( response => response.json())
+    .then(
+        // handle the result
+        (result) => {
+            this.setState({
+                planet : result.results
+            });
+            console.log(result)
+        },
+
+    )
+}
+
+    
+  
+render() {
+  const {planet} = this.state;
+      return(
+          <div className="cardBox">
+            
+              {
+                  planet.map(planet => (
+                      <div key={planet.id} className="card"  align="start">
+                              <h2 key={planet.id} id={planet.climate}>{planet.name}</h2>
+                              <table>
+                                <tr >
+                                  <td> 
+                                  Rotation Period: {planet.rotation_period}
+                                  </td>
+                                
+                                  <td> 
+                                  Orbital Period: {planet.orbital_period}
+                                  </td>
+                               
+                                  <td> 
+                                  Population: 
+                                 
+                                  {planet.population}
+                                  </td>
+                                </tr>
+                              </table>
+                            
+
+                      </div>
+                  ))
+              }
+             
+          </div>
+      );
+     
+}
+  
+}
+  
+  export { PlanetName };
+  
+
+
+
+
+
+
+
+
+
